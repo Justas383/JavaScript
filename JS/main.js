8bbb4766b9d33ce5963a959c1ff9e5a45f541ce4
Justas383 <74533002+Justas383@users.noreply.github.com>
2021-02-09 19:50:16 +0200

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,75 @@
+/*for (i=0; i<7; i++){
+    console.log('Iteration count:', i);
+}
+console.log("loop is finished"); */
+
+
+
+
+//const vardai = ['Jonas', 'Mantas', 'Koste', 'Mindaugas', 'Justas', 'Oleg'];
+//let i = 0;
+//while (i < vardai.length){
+//    console.log(`Zmogaus vardas yra ${vardai[i]}`);
+//    i++
+//}
+
+/*let price = 200;
+
+
+if (price > 220){
+console.log('This is too expensive')
+} else{
+    console.log('we can buy this');
+} */
+
+/*let pass = 'hsadasdasdasdasda';
+
+if (pass.length < 5){
+    console.log('Password is too short');
+} else if(pass.length > 12 && pass.includes('@')){
+    console.log('Password is strong');
+}else{
+    console.log('Password lenght is enough');
+}*/ 
+
+/* const vardai = ['Jonas', 'Mantas', 'Koste','', 'Mindaugas', 'Justas', 'Oleg'];
+let i = 0;
+while (i < vardai.length){
+    if(vardai[i]=== 'Nevykelis'){
+        continue;
+    }
+    console.log(`Zmogaus vardas yra ${vardai[i]}`);
+    i++;
+    if (vardai[i] === 'Mindaugas'){
+        console.log('Mindaugas yra karaliukas')
+        break;}
+}
+
+function speak(){
+    console.log('hello there');
+}
+
+speak();
+
+const greitis = function(a, b){
+    let greitis = a / b;
+    console.log(greitis);
+}
+
+greitis(1000, 60);
+
+const suPvm = (kaina) => (kaina/100) * 21 + kaina;
+
+const a = suPvm(100);
+console.log(a);
+
+
+
+let people = ['Jared','Mike', 'Johny', 'Belicia', 'Maggie'];
+
+people.forEach(function(person){
+    console.log(person);
+});
+*/ 
+
+
