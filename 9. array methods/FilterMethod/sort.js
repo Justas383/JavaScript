91a694c6e2c551adba7ef9d023a69f69b883898d
Justas383 <74533002+Justas383@users.noreply.github.com>
2021-02-10 23:41:30 +0200

    simplified players sorting

--- 9. array methods/FilterMethod/sort.js
+++ 9. array methods/FilterMethod/sort.js
@@ -6,6 +6,13 @@ console.log(names);
 
 const scores = [10, 50, 20, 5, 35, 70, 45];
 
+// scores.sort();
+// scores.reverse();
+// console.log(scores);
+
+scores.sort((a,b) => a - b);
+console.log(scores);
+
 
 const players = [
 {names: "mario", score: 20},
@@ -15,11 +22,14 @@ const players = [
 {names: "shaun", score: 70},
 ]
 
-players.sort((a,b) => {
-    if(a.score > b.score){
-        return -1;
-    } else if (b.score > a.score){
-        return 1;
-    } else {return 0};
-});
+// players.sort((a,b) => {
+//     if(a.score > b.score){
+//         return -1;
+//     } else if (b.score > a.score){
+//         return 1;
+//     } else {return 0};
+// });
+
+players.sort((a,b) => b.score - a.score);
+
 console.log(players);
